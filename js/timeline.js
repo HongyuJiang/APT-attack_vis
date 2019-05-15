b5759f7b3280eebbcadb88344a486637305a06ec
Hongyu.Jiang <1230223112@stu.cjlu.edu.cn>
2019-05-16 07:32:54 +0800

    update timeline

--- js/timeline.js
+++ js/timeline.js
@@ -13,19 +13,19 @@ function createTimeline(dataset, dateDict){
 			if (bins[time_key][service] != undefined){
 				
 				bins[time_key][service] += 1
-				bins[time_key]['counter'] += 1
+				
 			} 
 			else{
 				
 				bins[time_key][service] = 1
-				bins[time_key]['counter'] += 1
+
 			}
 		} 
 		else{
 			
 			bins[time_key] = {}
 			bins[time_key][service] = 1
-			bins[time_key]['counter'] = 1
+	
 		}
 		
 	})
@@ -36,7 +36,11 @@ function createTimeline(dataset, dateDict){
 		
 		let meta = bins[key]
 		
-		bins_list.push({'key':key,'stats':meta})
+		for (let service in meta){
+			
+			bins_list.push({'key':key,'stats':{'service':service,'value':meta[service]}})
+		}
+		
 	}
 
 	
@@ -45,40 +49,217 @@ function createTimeline(dataset, dateDict){
 	
 	let margin = ({top: 20, right: 0, bottom: 30, left: 40})
 	
-	x = d3.scaleBand()
+	/*x = d3.scaleBand()
 		.domain(bins_list.map(d => d.key))
 		.range([margin.left, width - margin.right])
-		.padding(0.2)
+		.padding(0.2)*/
+		
+	let min_date = d3.min(bins_list, (function(d){
+			
+			let date_arr = d.key.split('M-')
+			
+			let date = new Date('1990-01-01 00:' + date_arr[0] + ':' + date_arr[1])
+			
+			return date
+		}))
+		
+	let max_date = d3.max(bins_list, (function(d){
+			
+			let date_arr = d.key.split('M-')
+			
+			let date = new Date('1990-01-01 00:' + date_arr[0] + ':' + date_arr[1])
+			
+			return date
+		}))
+		
+	x = d3.scaleTime()
+		.domain([min_date, max_date])
+		.range([margin.left, width - margin.right])
 	
 	y = d3.scaleLinear()
-		.domain([0, d3.max(bins_list, d => d.stats.counter)]).nice()
+		.domain([0, d3.max(bins_list, d => d.stats.value)]).nice()
 		.range([height - margin.bottom, margin.top])
 	
 	const svg = d3.select('#timeline')
 		.append('svg')
 		.attr('width', width)
 		.attr('height', height)
+		
+	svg.append("g")
+	.attr("class", "brush")
+	.call(d3.brushX()
+	    .extent([[0, margin.top], [width, height - margin.bottom]])
+	    .on("end", brushended));
+		
 	  
-	let rectGroup = svg.selectAll('rectGroup')
+	let rectGroup = svg.selectAll('.rectGroup')
 		.data(bins_list)
 		.enter()
 		.append("g")
 		.on('click', function(d){
 			
-			let date = d.key
+			filterNetwork(d)
+				
+		})
+		
+	
+	rectGroup.append('circle')
+	      .attr("fill", d => d.stats.service == 'tcp' ? '#FF8C64' : '#666')
+	      .attr("cx", function(d){
+	      			  
+	      		let date_arr = d.key.split('M-')
+	      			  
+	      		let date = new Date('1990-01-01 00:' + date_arr[0] + ':' + date_arr[1])
+				
+				console.log(d)
+	      			  
+	      		return x(date)
+				
+	      })
+		  .attr('opacity', 0.5)
+	      .attr("cy", d => y(d.stats.value))
+	      .attr('stroke-width', 2)
+	      .attr('stroke', 'black')
+	      .attr("r", 5);
+		  
+	xAxis = g => g
+		.attr("transform", `translate(0,${height - margin.bottom})`)
+		.call(d3.axisBottom(x).tickSizeOuter(0).ticks(10))
+
+	yAxis = g => g
+		.attr("transform", `translate(${margin.left},0)`)
+		.call(d3.axisLeft(y))
+		.call(g => g.select(".domain").remove())
+	  
+	svg.append("g")
+	    .call(xAxis);
+		
+	svg.selectAll('.tick text')
+		.attr('opacity', function(d,i){
+			return i%2 == 0 ? 0 : 1
+		})
+		
+	svg.append("g")
+	    .call(yAxis);
+		
+	
 			
-			console.log(date)
+	function brushended() {
+		
+		let focus = []
+		
+		if (!d3.event.sourceEvent) return; // Only transition after input
+		if (!d3.event.selection) return; // Ignore empty selections.
+		var d0 = d3.event.selection;
+		
+		svg.selectAll('circle')
+		.attr('opacity', function(d){
+			
+			let pos_x = parseFloat(d3.select(this).attr('cx'))
+			
+			if(pos_x <= d0[1] && pos_x >= d0[0]){
+				focus.push(d)
+				return '1'
+			}
+			else
+				return '0.3'
+		})
+		
+		console.log(focus)
+		
+		filterGroupNetwork(focus)
+		
+	}
+	
+	function filterNetwork(d){
+		
+		let date = d.key
+		
+		let data_arr = date.split('M-')
+		
+		let stamp = '' + (data_arr[0] * 60 + parseInt(data_arr[1]))
+		
+		d3.selectAll('.link')
+			.attr('opacity', 0)
+			
+		let port_service = {}
+			
+		dateDict[stamp].forEach(function(d){
+			
+			let sourceName = 'N' + d.source.replace(/\./g,'+')
+			let targetName = 'N' + d.target.replace(/\./g,'+')
+			
+			let pathName = sourceName + '-' + targetName
+			
+			let pathSelector = '[id="' + pathName + '"]'
+			
+			d3.select(pathSelector).attr('opacity', 1)
+			
+			let name = d.sourcePort + ',' + d.targetPort
+			
+			if (port_service[targetName] != undefined){
+				
+				if(port_service[targetName][name] != undefined){
+					
+					port_service[targetName][name] += 1
+				}
+				else{
+					
+					port_service[targetName][name] = 1
+				}
+			}
+			else{
+				
+				port_service[targetName] = {}
+				port_service[targetName][name] = 1
+				
+			}
+			
+			
+			d3.select('#container').selectAll('.linkLabel').remove()
+			
+			
+			for(let target in port_service){
+				
+				let target_position = d3.select('[id="' + target + '"]').attr('transform')
+				
+				let index = 2
+				
+				for (let link in port_service[target]){
+					
+					index += 1
+					
+					d3.select('#container').select('svg')
+					.append('text')
+					.attr('class','linkLabel')
+					.attr('transform', target_position)
+					.attr('y', index * 18)
+					.attr('x', -50)
+					.text(link.replace(',',' -> '))
+					
+				}
+			}
+			
+				
+		})
+		
+	}
+	
+	function filterGroupNetwork(dateGroup){
+		
+		let port_service = {}
+		
+		d3.selectAll('.link')
+			.attr('opacity', 0)
+		
+		
+		dateGroup.forEach(function(d){
+			
+			let date = d.key
 			
 			let data_arr = date.split('M-')
 			
 			let stamp = '' + (data_arr[0] * 60 + parseInt(data_arr[1]))
-			
-			console.log(dateDict[stamp])
-			
-			d3.selectAll('.link')
-				.attr('opacity', 0)
-				
-			let port_service = {}
 				
 			dateDict[stamp].forEach(function(d){
 				
@@ -136,45 +317,13 @@ function createTimeline(dataset, dateDict){
 					}
 				}
 				
-		
+					
 			})
 			
-				
 		})
 		
-	rectGroup.append('rect')
-	      .attr("fill", "#FF8C64")
-	      .attr("x", d => x(d.key))
-	      .attr("y", d => y(d.stats.counter))
-	      .attr("height", d => y(0) - y(d.stats.counter))
-	      .attr("width", x.bandwidth());
-		  
-	rectGroup.append('rect')
-	      .attr("fill", "#A3A1A8")
-	      .attr("x", d => x(d.key))
-	      .attr("y", d => y(d.stats['udp']))
-	      .attr("height", d => d.stats['udp'] ? y(0) - y(d.stats['udp']) : 0)
-	      .attr("width", x.bandwidth());
-		  
-	xAxis = g => g
-		.attr("transform", `translate(0,${height - margin.bottom})`)
-		.call(d3.axisBottom(x).tickSizeOuter(0).ticks(10))
+		
+		
+	}
 
-	yAxis = g => g
-		.attr("transform", `translate(${margin.left},0)`)
-		.call(d3.axisLeft(y))
-		.call(g => g.select(".domain").remove())
-	  
-	svg.append("g")
-	    .call(xAxis);
-		
-	svg.selectAll('.tick text')
-		.attr('opacity', function(d,i){
-			return i%2 == 0 ? 0 : 1
-		})
-		
-	svg.append("g")
-	    .call(yAxis);
-			
-	  
 }
